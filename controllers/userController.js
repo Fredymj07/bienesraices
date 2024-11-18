af40047653cc4c496cc82cb36dc8dcadbdb93e0c
Fredy Montaña <fredy8462@hotmail.com>
2024-11-18 05:48:16 -0500

    :sparkles: Implementación de Tailwind CSS como herramienta para la definición de las vistas frontend de la aplicación. Creación de las vistas y formularios correspondientes a Iniciar Sesión, Crear Cuenta y Recordar Contraseña.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,23 @@
 const formLogin = (req, res) => {
     res.render('auth/login', {
-        
+        page: 'Login'
     })
 }
 
 const formRegister = (req, res) => {
     res.render('auth/register', {
-        
+        page: 'Create account'
+    })
+}
+
+const formForgotPassword = (req, res) => {
+    res.render('auth/forgot-password', {
+        page: 'Forgot password'
     })
 }
 
 export {
     formLogin,
-    formRegister
+    formRegister,
+    formForgotPassword
 }
